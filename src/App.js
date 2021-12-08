de5f12cb11687d0ace6b09a3ce682944f26e7e8b
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2021-12-07 19:28:31 -0700

    cleaned up a little code

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData)
   
+  
 
   const likePost = postId => {
     
